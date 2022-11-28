2ddc49b76519d1589c3df9ea7d8e6917666af8dc
mejiabrayan <learnwithbrayan@gmail.com>
2022-11-28 06:31:02 -0800

    updated chat ui and implemented firebase

--- components/Chat.js
+++ components/Chat.js
@@ -1,222 +1,186 @@
-
 import React from 'react';
-import { View, Text, TextInput, Button, KeyboardAvoidingView, Platform } from 'react-native';
+import {
+    StyleSheet,
+    View,
+    Text,
+    Platform,
+    KeyboardAvoidingView
+} from 'react-native';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+import 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+// import NetInfo from '@react-native-community/netinfo';
+// import CustomActions from './CustomActions';
+// import { ActionSheetProvider } from '@expo/react-native-action-sheet';
+// import MapView from 'react-native-maps';
 
 const firebase = require('firebase');
 require('firebase/firestore');
 
 /**
  * Represents a Chat component
- * @function Chat - A React component
- * @param {object} route - An object that contains information about the route that was navigated to
- * @param {object} navigation - An object that contains information about the navigation state
+ * @function Chat - A React class component
  */
+export default class Chat extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            messages: [],
+            uid: 0,
+            user: {
+                _id: '',
+                avatar: '',
+                name: '',
+            },
+            loggedInText: 'Please wait, you are getting logged in',
+            image: null,
+            location: null,
+            isConnected: false,
+        };
 
-export default function Chat({ navigation, route }) {
 
-    const [uid, setUid] = React.useState('');
-    const [loggedIn, setLoggedIn] = React.useState(false);
-    const [connected, setConnected] = React.useState(false);
-    const [messages, setMessages] = React.useState([]);
+        /**
+         * Firebase configuration
+         * @function firebaseConfig - firebase configuration
+         * @param {string} apiKey - firebase apiKey
+         * @param {string} authDomain - firebase authDomain
+         * @param {string} projectId - firebase projectId
+         * @param {string} storageBucket - firebase storageBucket
+         * @param {string} messagingSenderId - firebase messagingSenderId
+         * @param {string} appId - firebase appId
+         * @param {string} measurementId - firebase measurementId
+         */
 
-    const firebaseConfig = {
+        if (!firebase.apps.length) {
+            firebase.initializeApp({
+                apiKey: "AIzaSyAVs5A4r-j6Ala90zt3whILp1LUPfNcr3s",
 
-        apiKey: "AIzaSyAVs5A4r-j6Ala90zt3whILp1LUPfNcr3s",
-        authDomain: "birdychat-app.firebaseapp.com",
-        projectId: "birdychat-app",
-        storageBucket: "birdychat-app.appspot.com",
-        messagingSenderId: "914399359321",
-        appId: "1:914399359321:web:be3d0beeef37ddb52fdc48",
-        measurementId: "G-TKK1CVHTP7"
+                authDomain: "birdychat-app.firebaseapp.com",
 
-    };
+                projectId: "birdychat-app",
 
-    if (!firebase.apps.length) {
-        firebase.initializeApp(firebaseConfig);
+                storageBucket: "birdychat-app.appspot.com",
+
+                messagingSenderId: "914399359321",
+
+                appId: "1:914399359321:web:be3d0beeef37ddb52fdc48",
+
+                measurementId: "G-TKK1CVHTP7"
+
+            });
+        }
+
+        this.referenceChatMessages = firebase.firestore().collection('messages');
     }
 
-    const referenceChatMessages = firebase.firestore().collection('messages');
 
-    // @type function
-    // @name onCollectionUpdate
-    // @description This function takes in a querySnapshot as an argument and sets the value of the messages state to the value of the argument
-    function onCollectionUpdate(querySnapshot) {
+    componentDidMount() {
+        const name = this.props.route.params.name;
+        this.props.navigation.setOptions({ title: name });
+
+        this.referenceChatMessages = firebase.firestore().collection("messages");
+        this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+            if (!user) {
+                firebase.auth().signInAnonymously();
+            }
+            this.setState({
+                uid: user?.uid,
+                messages: [],
+            });
+            this.unsubscribe = this.referenceChatMessages
+                .orderBy("createdAt", "desc")
+                .onSnapshot(this.onCollectionUpdate);
+        });
+    }
+
+    onSend(messages = []) {
+        this.setState(
+            (previousState) => ({
+                messages: GiftedChat.append(previousState.messages, messages),
+            }),
+            () => {
+                const message = messages[0];
+                this.addMessage(message);
+            }
+        );
+    }
+
+    /**
+     * @function onCollectionUpdate
+     * @param {object} querySnapshot - firebase querySnapshot
+     * @param {object} error - firebase error
+     * @param {object} messages - firebase messages
+     * @decription - updates the state of the messages
+     */
+
+    onCollectionUpdate = (querySnapshot) => {
         const messages = [];
         querySnapshot.forEach((doc) => {
-            const data = doc.data();
+            let data = doc.data();
+            console.log(doc);
             messages.push({
                 _id: data._id,
                 text: data.text,
-                createdAt: data.timeOfCreation.toDate(),
-                user: {
-                    _id: data.user._id,
-                    name: data.user.name,
-                },
+                createdAt: data.createdAt.toDate(),
+                user: data.user,
             });
         });
-        setMessages(messages);
-    }
+        this.setState({ messages });
+    };
 
-
-    /** 
-     * @type function
-     * @name addMessage
-     * @description This function takes in a message as an argument and adds the message to the database
-     */
-
-    function addMessage(message) {
-        referenceChatMessages.add(message);
-    }
-    /**
-     * @type useEffect
-     * @description This is a function that takes in a callback function as an argument and returns a function that will be called when the component is unmounted
-     * @param {function} callback - A function that will be called when the component is unmounted
-     * @param {array} dependencies - An array of values that the effect depends on
-     * @returns {function} - A function that will be called when the component is unmounted
-     */
-
-    React.useEffect(() => {
-        const { name, backgroundColor, textColor } = route.params;
-        navigation.setOptions({ title: name });
-        navigation.setOptions({ backgroundColor: backgroundColor });
-        navigation.setOptions({ textColor: textColor });
-        firebase.auth().onAuthStateChanged((user) => {
-            if (!user) {
-                firebase.auth().signInAnonymously();
-            }
-            setUid(user.uid);
-            setLoggedIn(true);
+    addMessage(message) {
+        this.referenceChatMessages.add({
+            _id: message._id,
+            createdAt: message.createdAt,
+            text: message.text,
+            user: message.user,
+            uid: this.state.uid,
         });
-        referenceChatMessages.orderBy('timeOfCreation', 'desc').onSnapshot(onCollectionUpdate);
-    }, []);
-
-    /**
-    * @type function
-    * @name getMessages
-    * @description This function takes in a message as an argument and returns a message
-    */
-
-    function getMessages() {
-        return messages;
     }
 
     /**
-     * @type function
-     * @saveMessages
-     * @description This function takes in a message as an argument and saves the message to the database
-     * @param {object} message - An object that contains the message
-     * @returns {object} - An object that contains the message
-     * @example
-     * saveMessages(message)
-     * // returns message
+     * @function componentWillUnmount
+     * @description - stops listening to authentication changes and messages changes in the database
      */
-    function saveMessages(message) {
-        setMessages(message);
+    componentWillUnmount() {
+        this.unsubscribe();
+        this.authUnsubscribe();
     }
 
 
-    /**
-    * @type function
-    * @name deleteMessages
-    * @description This function takes in a message as an argument and deletes the message from the database
-    * @param {object} message - An object that contains the message
-     */
-    function deleteMessages(message) {
-        setMessages(message);
-    }
-
-   
-
-    // @type function
-    // @name onSend
-    // @description This function takes in a messages as an argument and sets the value of the messages state to the value of the argument
-    function onSend(messages = []) {
-        setMessages(GiftedChat.append(messages, messages));
-        addMessage(messages[0]);
-    }
-
-
-    /*
-    * @type function
-* @name authSubscribe
-* @description This function takes in a messages as an argument and sets the value of the messages state to the value of the argument
-*/
-
-    function authSubscribe() {
-        firebase.auth().onAuthStateChanged((user) => {
-            if (!user) {
-                firebase.auth().signInAnonymously();
-            }
-            setUid(user.uid);
-            setLoggedIn(true);
-        });
-
-        const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
-            if (!user) {
-                firebase.auth().signInAnonymously();
-            }
-            setUid(user.uid);
-            setLoggedIn(true);
-        }
-        );
-        return unsubscribe;
-    }
-
-    /*
-   * @type useEffect
-    * @name useEffect
-    * @description This function takes in a messages as an argument and sets the value of the messages state to the value of the argument
-    * @param {function} authSubscribe - A function that subscribes to the authentication state
-    * @param {function} getMessages - A function that gets the messages from the database
-    */
-
-    React.useEffect(() => {
-        authSubscribe();
-        getMessages();
-    }, []);
-
-
-
-    /**
-     * @type function
-     * @name renderBubble
-     * @description This function takes in a props as an argument and returns a JSX element
-     *  @param {object} props - An object that contains information about the props
-     * @returns {JSX} - A JSX element
-     */
-
-    function renderBubble(props) {
+    //Bubble customization
+    renderBubble(props) {
         return (
             <Bubble
                 {...props}
                 wrapperStyle={{
+                    left: {
+                        backgroundColor: "#fff",
+                    },
                     right: {
-                        backgroundColor: '#000'
-                    }
-                }}
-                textStyle={{
-                    right: {
-                        color: textColor
-                    }
+                        backgroundColor: "#000",
+                    },
                 }}
             />
         );
     }
 
-    const { name, backgroundColor, textColor } = route.params;
-    
-    return (
-        <View style={{ flex: 1, backgroundColor: backgroundColor }}>
-            <GiftedChat
-                messages={messages}
-                onSend={messages => onSend(messages)}   
-                user={{
-                    _id: uid,
-                    name: name,
-                }}
-                renderBubble={renderBubble}
-            />
-        </View>
-    );
+    render() {
+        const { textColor: textColor, backgroundColor: backgroundColor } = this.props.route.params;
+        return (
+            <View style={{ flex: 1, backgroundColor: backgroundColor, textColor: textColor }}>
+                <GiftedChat
+                    renderBubble={this.renderBubble.bind(this)}
+                    messages={this.state.messages}
+                    onSend={(messages) => this.onSend(messages)}
+                    user={{
+                        _id: 1,
+                    }}
+                />
+                {Platform.OS === "android" ? (
+                    <KeyboardAvoidingView behavior="height" />
+                ) : null}
+            </View>
+        );
+    }
 }
