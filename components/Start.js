93ef2d1dc23f6922d21310822774685562afb48a
mejiabrayan <learnwithbrayan@gmail.com>
2022-11-28 06:31:50 -0800

    added accessibility

--- components/Start.js
+++ components/Start.js
@@ -6,22 +6,22 @@ import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 
 
+/**
+ * Represents a Start component 
+ * @function Start - A React component
+ * @param {object} navigation - An object that contains information about the navigation state
+ * @param {object} route - An object that contains information about the route that was navigated to
+ * @returns {JSX.Element} - JSX that renders the Start component
+ */
 
-// @ This is a function component that takes in props as an argument and returns JSX to be rendered
-export default function Start({ navigation , route}) {
+export default function Start({ navigation }) {
 
-    // @ type Hook
-    // @description This is a state variable that is initialized to #fff @ //
     const [backgroundColor, setBackgroundColor] = React.useState('#1c2026');
 
-    // @ type Hook
-    // @description textColor is a state variable that is initialized to '#000' @ //
     const [textColor, setTextColor] = React.useState('#fdfdfe');
-    // @ type Hook
-    // @description This is a state variable that is initialized to an empty string @ //
+
     const [text, setText] = React.useState('');
 
-    
     let [fontsLoaded] = useFonts({
         'Poppins': require('./assets/fonts/Poppins-Regular.ttf'),
         'Poppins-Bold': require('./assets/fonts/Poppins-Bold.ttf'),
@@ -58,7 +58,7 @@ export default function Start({ navigation , route}) {
 
     return (
         <View
-            style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: backgroundColor, color: textColor}}
+            style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: backgroundColor, color: textColor }}
             accessible={true}
             accessibilityLabel='Start'
             accessibilityHint='A screen where you can enter your name and choose a background color'
@@ -67,15 +67,18 @@ export default function Start({ navigation , route}) {
 
             <Text style={{ fontSize: 45, marginBottom: 50, fontWeight: '400', color: textColor, fontFamily: 'Poppins-Bold' }}> Birdy Chat App</Text>
             <Text style={{ fontSize: 25, marginBottom: 50, color: textColor }}>start a chat!</Text>
-            {/* handle change */}
+
             <TextInput style={{ height: 40, width: 200, borderColor: 'gray', borderWidth: 1, marginBottom: 50, color: textColor, padding: 5 }} onChangeText={handleChange} value={text} placeholder='type name' placeholderTextColor="#fdfdfe" />
             <Text style={{ fontSize: 20, marginBottom: 50, color: textColor, fontWeight: '400' }}>Choose a background color:</Text>
-            <View style={{ flexDirection: 'row'}}>
-                <View style={{ backgroundColor: '#1c2026', width: 50, height: 50, marginRight: 10, borderRadius: 50 }} accessible={true} accessibilityLabel='Dark' accessibilityHint='Choose a dark background' accessibilityRole='button' accessibilityState={{selected: backgroundColor === '#b9c6ae'}} onTouchEnd={() => {  setBackgroundColor('#1c2026'); setTextColor(textColor) }} />
-                <View style={{ backgroundColor: '#1DA1F2', width: 50, height: 50, marginRight: 10, borderRadius: 50 }}  accessible={true} accessibilityLabel='Light Blue' accessibilityHint='Choose a Light Blue background' accessibilityRole='button' accessibilityState={{selected: backgroundColor === '#1DA1F2'}}  onTouchEnd={() => { setBackgroundColor('#1DA1F2'); setTextColor(textColor) }} />
-                <View style={{ backgroundColor: '#8A95A5', width: 50, height: 50, marginRight: 10, borderRadius: 50 }}  onTouchEnd={() => { setBackgroundColor('#8A95A5'); setTextColor(textColor) }} />
+            <View style={{ flexDirection: 'row' }}>
+                <View style={{ backgroundColor: '#1c2026', width: 50, height: 50, marginRight: 10, borderRadius: 50 }} accessible={true} 
+                accessibilityLabel='Dark' accessibilityHint='Choose a dark background' 
+                accessibilityRole='button' accessibilityState={{ selected: backgroundColor === '#b9c6ae' }}
+                 onTouchEnd={() => { setBackgroundColor('#1c2026'); setTextColor(textColor) }} />
+                <View style={{ backgroundColor: '#1DA1F2', width: 50, height: 50, marginRight: 10, borderRadius: 50 }} accessible={true} accessibilityLabel='Light Blue' accessibilityHint='Choose a Light Blue background' accessibilityRole='button' accessibilityState={{ selected: backgroundColor === '#1DA1F2' }} onTouchEnd={() => { setBackgroundColor('#1DA1F2'); setTextColor(textColor) }} />
+                <View style={{ backgroundColor: '#8A95A5', width: 50, height: 50, marginRight: 10, borderRadius: 50 }} onTouchEnd={() => { setBackgroundColor('#8A95A5'); setTextColor(textColor) }} />
                 <View style={{ backgroundColor: '#B9C6AE', width: 50, height: 50, marginRight: 10, borderRadius: 50 }} onTouchEnd={() => { setBackgroundColor('#B9C6AE'); setTextColor(textColor) }} />
-                </View>
+            </View>
             <View style={{ marginTop: 50, width: 200, height: 50, borderRadius: 50, backgroundColor: '#757083', justifyContent: 'center', alignItems: 'center' }} onTouchEnd={handlePress}>
                 <Text
                     style={{ fontSize: 20, color: textColor, fontWeight: 'bold', fontFamily: 'Poppins-Bold' }}
@@ -88,7 +91,3 @@ export default function Start({ navigation , route}) {
         </View>
     );
 }
-
-
-
-
