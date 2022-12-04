9808f6343387d3191a3f8026d6e5a98a5a8977ad
mejiabrayan <learnwithbrayan@gmail.com>
2022-12-04 11:24:10 -0800

    fixed potential bug

--- components/Chat.js
+++ components/Chat.js
@@ -73,26 +73,109 @@ export default class Chat extends React.Component {
     }
 
     /**
-     * @function onCollectionUpdate
-     * @param {object} querySnapshot - firebase querySnapshot
-     * @param {object} error - firebase error
-     * @param {object} messages - firebase messages
-     * @decription - updates the state of the messages
+     * @name getMessages
+     * @description gets messages from the firebase database
      */
+    async getMessages() {
+        let messages = "";
+        try {
+            messages = (await AsyncStorage.getItem("messages")) || [];
+            console.log("called set state for messages", messages);
+            this.setState({
+                messages: JSON.parse(messages),
+            });
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
 
+    componentDidMount() {
+        let name = this.props.route.params.name;
+        this.props.navigation.setOptions({ title: name });
+        this.getMessages();
+        NetInfo.fetch().then((connection) => {
+            if (connection.isConnected) {
+                this.setState({
+                    isConnected: true,
+                });
+            } else {
+                this.setState({
+                    isConnected: false,
+                });
+            }
+        });
+
+        this.referenceChatMessages = firebase.firestore().collection("messages");
+        this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+            if (!user) {
+                firebase.auth().signInAnonymously();
+            }
+            this.setState({
+                uid: user?.uid,
+                messages: [],
+                user: {
+                    _id: user.uid,
+                    name: name,
+                    avatar: "https://placeimg.com/140/140/any",
+                },
+            });
+            this.unsubscribe = this.referenceChatMessages
+                .orderBy("createdAt", "desc")
+                .onSnapshot(this.onCollectionUpdate);
+        });
+    }
+
+    onSend(messages = []) {
+        this.setState(
+            (previousState) => ({
+                messages: GiftedChat.append(previousState.messages, messages),
+            }),
+            () => {
+                const message = messages[0];
+                this.addMessage(message);
+                this.saveMessages();
+            }
+        );
+    }
+
+    async saveMessages() {
+        try {
+            await AsyncStorage.setItem(
+                "messages",
+                JSON.stringify(this.state.messages)
+            );
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    async deleteMessages() {
+        try {
+            await AsyncStorage.removeItem("messages");
+            this.setState({
+                messages: [],
+            });
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    //Get the data
     onCollectionUpdate = (querySnapshot) => {
+        if (!this.state.isConnected) return;
         const messages = [];
-        // Go through each document
+        // go through each document
         querySnapshot.forEach((doc) => {
-            // Get QueryDocumentSnapshot's data
+            // get the QueryDocumentSnapshot's data
             let data = doc.data();
             messages.push({
                 _id: data._id,
-                text: data.text || '',
+                text: data.text,
                 createdAt: data.createdAt.toDate(),
                 user: {
                     _id: data.user._id,
                     name: data.user.name,
+                    avatar: data.user.avatar || "",
                 },
                 image: data.image || null,
                 location: data.location || null,
@@ -103,146 +186,32 @@ export default class Chat extends React.Component {
         });
     };
 
-
-    /**
-     *  @name getMessages
-     * @description - gets messages from the database
-     */
-    async getMessages() {
-        let messages = '';
-        try {
-            messages = await AsyncStorage.getItem('messages') || [];
-            this.setState({
-                messages: JSON.parse(messages)
-            });
-        } catch (error) {
-            console.log(error.message);
-        }
-    };
-
-    /**
-     * @function saveMessages
-     * @description - saves messages to the database
-     */
-    async saveMessages() {
-        try {
-            await AsyncStorage.setItem('messages', JSON.stringify(this.state.messages));
-        } catch (error) {
-            console.log(error.message);
-        }
-    }
-
-    async deleteMessages() {
-        try {
-            await AsyncStorage.removeItem('messages');
-            this.setState({
-                messages: [] // clear the messages
-            });
-        } catch (error) {
-            console.log(error.message);
-        }
-    }
-
-    componentDidMount() {
-        const name = this.props.route.params.name;
-        this.props.navigation.setOptions({ title: name });
-        this.getMessages();
-
-        NetInfo.fetch().then(connection => {
-            if (connection.isConnected) {
-                this.setState({
-                    isConnected: true,
-                });
-                console.log('online');
-
-
-                //Anonymous user authentication 
-                this.referenceChatMessages = firebase.firestore().collection('messages');
-
-                this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
-                    if (!user) {
-                        firebase.auth().signInAnonymously();
-                    }
-                    this.setState({
-                        uid: user.uid,
-                        messages: [],
-                        user: {
-                            _id: user.uid,
-                            name: name,
-                            avatar: 'https://placeimg.com/140/140/any',
-                        },
-                    });
-                    this.unsubscribe = this.referenceChatMessages
-                        .orderBy('createdAt', 'desc')
-                        .onSnapshot(this.onCollectionUpdate);
-                    this.saveMessages();
-                });
-            }
-            // If user is offline --> load & display messages from asyncStorage
-            else {
-                this.setState({
-                    isConnected: false,
-                });
-                console.log('offline');
-                this.getMessages();
-            }
-        })
-    }
-    /**
-     * @function addMessages
-     * @param {object} messages - firebase messages
-     * @description - adds messages to the database
-     * @returns {object} - firebase messages
-     */
-    addMessages = () => {
-        const message = this.state.messages[0];
+    addMessage(message) {
         this.referenceChatMessages.add({
             uid: this.state.uid,
             _id: message._id,
-            text: message.text || '',
             createdAt: message.createdAt,
-            user: message.user,
+            text: message.text || "",
+            user: this.state.user,
             image: message.image || null,
             location: message.location || null,
         });
     }
 
-
-    /**
-     * @function onSend
-     * @description - sends messages to the database
-     * @param {array} messages - array of messages
-     * @param {string} text - message text
-    */
-    onSend(messages = []) {
-        this.setState(previousState => ({
-            messages: GiftedChat.append(previousState.messages, messages),
-        }), () => {
-            this.saveMessages();
-            this.addMessages(this.state.messages[0]);
-            this.deleteMessages();
-        });
+    componentWillUnmount() {
+        this.authUnsubscribe();
+        this.unsubscribe();
     }
+
     /**
      * @function renderInputToolbar
      * @description - renders the input toolbar if the user is online
      */
-    
-     renderInputToolbar(props) {
+
+    renderInputToolbar(props) {
         if (this.state.isConnected === false) {
         } else {
-          return <InputToolbar {...props} />;
-        }
-      }
-
-    /**
-     * @function componentWillUnmount
-     * @description - stops listening to authentication changes and messages changes in the database
-     */
-    componentWillUnmount() {
-        if (this.isConnected) {
-            this.unsubscribe();
-            this.authUnsubscribe();
+            return <InputToolbar {...props} />;
         }
     }
 
@@ -295,27 +264,29 @@ export default class Chat extends React.Component {
         const { backgroundColor: backgroundColor } = this.props.route.params;
 
         return (
-            <View style={{ flex: 1, backgroundColor: backgroundColor}}>
-                <GiftedChat
-                    bottomOffset={getBottomSpace()}
-                    renderBubble={this.renderBubble.bind(this)}
-                    renderInputToolbar={this.renderInputToolbar.bind(this)}
-                    messages={this.state.messages}
-                    isConnected={this.state.isConnected}
-                    onSend={(messages) => this.onSend(messages)}
-                    user={{
-                        _id: this.state.uid,
-                        avatar: "https://placeimg.com/140/140/any",
+            <ActionSheetProvider>
+                <View style={{ flex: 1, backgroundColor: backgroundColor }}>
+                    <GiftedChat
+                        bottomOffset={getBottomSpace()}
+                        renderBubble={this.renderBubble.bind(this)}
+                        renderInputToolbar={this.renderInputToolbar.bind(this)}
+                        messages={this.state.messages}
+                        isConnected={this.state.isConnected}
+                        onSend={(messages) => this.onSend(messages)}
+                        user={{
+                            _id: this.state.uid,
+                            avatar: "https://placeimg.com/140/140/any",
 
-                    }}
-                    renderActions={this.renderCustomActions}
-                    renderCustomView={this.renderCustomView}
-                />
-                {/* Prevent hidden input field on Android */}
-                {Platform.OS === "android" ? (
-                    <KeyboardAvoidingView behavior="height" />
-                ) : null}
-            </View>
+                        }}
+                        renderActions={this.renderCustomActions}
+                        renderCustomView={this.renderCustomView}
+                    />
+                    {/* Prevent hidden input field on Android */}
+                    {Platform.OS === "android" ? (
+                        <KeyboardAvoidingView behavior="height" />
+                    ) : null}
+                </View>
+            </ActionSheetProvider>
         );
     }
 }
